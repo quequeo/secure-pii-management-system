9d5f70187116d67fe9a1504002ffb0d13634047d
Jaime García Méndez <jaimegarciamendez@gmail.com>
2025-12-20 21:41:30 -0500

    Setup Java Spring Boot project structure

---
+++ java-service/src/main/java/com/pii/validation/SsnValidationApplication.java
@@ -0,0 +1,12 @@
+package com.pii.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsnValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsnValidationApplication.class, args);
+    }
+}
