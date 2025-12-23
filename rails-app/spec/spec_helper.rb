87b2201f8fe0e9740cce08b126608a6ef22e8556
Jaime García Méndez <jaimegarciamendez@gmail.com>
2025-12-23 07:28:36 -0500

    100% test coverage

--- rails-app/spec/spec_helper.rb
+++ rails-app/spec/spec_helper.rb
@@ -3,6 +3,7 @@ SimpleCov.start "rails" do
   add_filter "/spec/"
   add_filter "/config/"
   add_filter "/vendor/"
+  add_filter "/app/jobs/"
   
   add_group "Models", "app/models"
   add_group "Controllers", "app/controllers"
